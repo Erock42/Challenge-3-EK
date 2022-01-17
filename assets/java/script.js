4d714927df8b469d9e341c3b24adb8ac0f9bc719
Erock FaQue <eric.kemerer@gmail.com>
2022-01-16 20:17:37 -0500

    prompts work, working on generation

--- assets/java/script.js
+++ assets/java/script.js
@@ -1,24 +1,39 @@
-// Assignment code here
-var passwordText = document.querySelector("#password");
+var length = Number(prompt("How long would you like your password to be?"));
+while (isNaN(length) || length < 8 || length > 128) length = Number(prompt("Length must be 8-128 characters. How long would you like your password to be?"));
 
- function writePassword() {
-    var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var passwordLength = 12;
-    var password = "";
+var uppers = confirm("Would you like to use uppercase letters?");
+var lowers = confirm("Would you like to use lowercase letters?");
+var numbers = confirm("Would you like to use numbers?");
+var symbols = confirm("Would you like to use special characters?");
 
- for (var i = 0; i <= passwordLength; i++) {
-   var randomNumber = Math.floor(Math.random() * chars.length);
-   password += chars.substring(randomNumber, randomNumber +1);
-  }
-        document.getElementById("password").value = password;
- }
+while (!uppers && !lowers && !numbers && !symbols) {
+  alert("You must select at least one character type!");
+  uppers = confirm("Would you like to use uppercase letters?");
+  lowers = confirm("Would you like to use lowercase letters?");
+  numbers = confirm("Would you like to use numbers?");
+  symbols = confirm("Would you like to use special characters?");
+}
 
-
-// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+generateBtn.addEventListener('click', function() {
+  writePassword();
+});
 
+function writePassword() {
+  var password = "";
+  var allowed = {};
+  if (uppers) password += (allowed.uppers = "QWERTYUIOPASDFGHJKLZXCVBNM");
+  if (lowers) password += (allowed.lowers = "qwertyuiopasdfghjklzxcvbnm");
+  if (numbers) password += (allowed.numbers = "1234567890");
+  if (symbols) password += (allowed.symbols = "!@#$%^&*(){}[]=<>/,.");
+
+  for (var i = password.length; i < length; i++) password += randomNumber(randomNumber(allowed)); 
+  var randomNumber = Math.floor(Math.random() * length);
+   password += writePassword(randomNumber, randomNumber +1);
+  
   
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+  document.getElementById("password").value =password;
+}
+
